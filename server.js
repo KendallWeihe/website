d5347e6c8aa0c7da832a07b212d34ee7d8ddd735
Kendall Weihe <kendallweihe@gmail.com>
2017-12-22 22:47:46 -0500

    added logging

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const util = require('util')
 const AWS = require('aws-sdk');
+const log = require('simple-node-logger').createSimpleLogger('info.log');
 
 var fs = require('fs');
 var https = require('https');
@@ -23,18 +24,24 @@ app.use(device.capture());
 var server = https.createServer( options, app );
 server.listen(443, function () {
     console.log( 'Express server listening on port ' + server.address().port );
+    log.info( 'Express server listening on port ' + server.address().port );
 });
 
 app.get('/', function(req, res) {
   console.log(req.headers);
+  log.info(req.headers);
   if (req.get('X-Forwarded-Proto') !== 'https') {
     console.log("Insecure, redirecting...");
+    log.info("Insecure, redirecting...");
     res.redirect('https://' + req.get('Host') + req.url);
   }
   else {
     console.log(req.connection.remoteAddress);
+    log.info(req.connection.remoteAddress);
     console.log(req.device);
+    log.info(req.device);
     console.log(req.device.type.toUpperCase());
+    log.info(req.device.type.toUpperCase());
     var device_type = req.device.type.toUpperCase();
     var device_type_enum = 0;
     if (device_type == "PHONE") {
